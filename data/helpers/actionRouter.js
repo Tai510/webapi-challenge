e0e14ff88127a3d9d05cafdac5679050acdd8d21
tashi woeser <tashiwoeser@tashis-MacBook-Pro.local>
2019-05-31 09:40:08 -0700

    put is now working

--- data/helpers/actionRouter.js
+++ data/helpers/actionRouter.js
@@ -9,9 +9,33 @@ router.get('/', (req, res) =>{
         res.status(200).json(action)
     })
     .catch( error => {
-        res.status(500).json({error:{message: " error getting data"}})
+        res.status(500).json({error:{message: " error getting data "}})
     })
 })
+
+router.post('/', (req, res) => {
+    const newAction = req.body 
+    dbaction.insert(newAction)
+    .then( action => {
+        res.status(200).json(action)
+    }).catch( error => {
+        res.status(500).json({error:{message: " error getting data "}})
+    })
+})
+
+router.put('/:id', (req, res) => {
+    const updateAct = req.body
+    const id = req.params.id
+
+    dbaction.update(id, updateAct)
+    .then( action => {
+        res.status(200).json(action)
+    })
+    .catch( err => {
+        res.status(500).json({ error: err, message:"could not update id"})
+    })
+})
+
 
 
 module.exports = router
