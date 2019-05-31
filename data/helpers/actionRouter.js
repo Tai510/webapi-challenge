767a7f841cf5317026b4176e4a933a9f8c2796b8
tashi woeser <tashiwoeser@tashis-MacBook-Pro.local>
2019-05-31 09:50:19 -0700

    delete is working now

--- data/helpers/actionRouter.js
+++ data/helpers/actionRouter.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router();
 const dbaction = require('./actionModel.js')
-// router.use(express.json())
+router.use(express.json())
 
 router.get('/', (req, res) =>{
     dbaction.get()
@@ -24,15 +24,34 @@ router.post('/', (req, res) => {
 })
 
 router.put('/:id', (req, res) => {
-    const updateAct = req.body
+    const updateAction = req.body
     const id = req.params.id
 
-    dbaction.update(id, updateAct)
+    dbaction.update(id, updateAction)
     .then( action => {
         res.status(200).json(action)
     })
     .catch( err => {
         res.status(500).json({ error: err, message:"could not update id"})
+    })
+})
+
+router.delete('/:id', (req, res) => {
+    const deleteAction = req.params.id
+    dbaction.remove(deleteAction)
+    .then( action => {
+        if(action) {
+            dbaction.remove(deleteAction).then(
+                removeAction => {
+                res.status(201).json(removeAction)
+                }
+            )
+        } else {
+            res.status(404).json({ error: err, message: "The user id does not exist"})
+        }
+    })
+    .catch(error => {
+        res.status(500).json({ message: "User could not be removed"})
     })
 })
 
